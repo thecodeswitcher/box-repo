4f7953475b30e579573e4c74393bc6eab4bf9094
Jonathan Ramirez <jramirezor.91@gmail.com>
2023-10-03 23:17:24 +0100

    Adding the .pem file

--- boxrepo/boxrepo/settings.py
+++ boxrepo/boxrepo/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+    
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
